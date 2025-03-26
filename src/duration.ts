5a950a1095d4b735c2a86f03cbdcb29056c576a3
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-26 09:01:05 +0100

    satisfy linter with boilerplate

--- src/duration.ts
+++ src/duration.ts
@@ -1,3 +1,4 @@
 export function formatDuration(seconds: number): string {
+  console.log(seconds);
   throw new Error("not implemented yet");
 }
--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -1,3 +1,4 @@
 export function play(player1: string, player2: string): number {
+  console.log(player1, player2);
   throw new Error("not implemented yet");
 }
